2a40b941e886e0effca6861e9ba18650856d7715
Alexey <alex.xenkok@gmail.com>
2020-03-22 22:18:09 +0200

    desktop ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
-console.log('Hi!');
+$(document).ready(function() {
+	// Nav
+	$("#page-nav").onePageNav({
+		currentClass: "nav-menu-item--active",
+		changeHash: false,
+		scrollSpeed: 400,
+		scrollThreshold: 0.6,
+		filter: "",
+		easing: "swing"
+	});
+
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		loop: true,
+		dots: true,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		autoplaySpeed: 1000
+	});
+});
